bff44f303bf8bd50f376c57e639228b7566a4e14
Juan Bta. Peris Fontestad <jbperfon@epsg.upv.es>
2024-04-03 20:06:45 +0200

    BarraDeVida Funciona
    La barra de vida funciona, si los enemigos llegan a la casa,la vida baja

---
+++ Assets/__TESTING__/BarraDeVida/comportamientoBarra.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vida : MonoBehaviour {
+  
+    public UnityEngine.UI.Image vida;
+
+    public float vidaActual = 100f;
+
+    private void OnCollisionEnter(Collision other){
+        
+        if(other.gameObject.tag== "enemigo1" || other.gameObject.tag== "enemigo2" || other.gameObject.tag== "enemigo3"){
+
+            Debug.Log("Hola perra");
+
+            vidaActual -= 25f;
+
+            
+            vida.fillAmount = vidaActual/100f;
+
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+}
