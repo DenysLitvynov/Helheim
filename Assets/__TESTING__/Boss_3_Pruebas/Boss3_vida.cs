71497b1cf1567abc687c80cafa53465081cb024d
arielbej <ariel507182003@gmail.com>
2024-04-03 17:33:07 +0200

    Ya toma daño differentes.

--- Assets/__TESTING__/Boss_3_Pruebas/Boss3_vida.cs
+++ Assets/__TESTING__/Boss_3_Pruebas/Boss3_vida.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Boss3_vida : MonoBehaviour
 {
     private Boss_Movimiento combate;
+    public float vida_maxima = 100f;
     public float vida = 100f; // La vida del enemigo
     public float daño_boss = 40f;
+    private Vida_aliado_dummy aliado;
 
     private void Start()
     {
@@ -17,24 +19,39 @@ public class Boss3_vida : MonoBehaviour
     {
         if (combate.esta_en_combate == true)
         {
-            Vida_aliado_dummy aliado = GameObject.FindObjectOfType<Vida_aliado_dummy>();
+            // Accede a la variable dps del aliado
             recibirDaño(aliado.dps);
         }
     }
 
     private void recibirDaño(float daño)
     {
-        if (combate.esta_en_combate==true)
+        if (combate.esta_en_combate == true)
         {
-            vida -= daño*Time.deltaTime;
+            vida -= daño * Time.deltaTime;
         }
         // Comprueba si la vida del enemigo ha llegado a 0
-        if (vida <= 0)
+        if (vida <= 0 || daño > vida_maxima) 
         {
             Destroy(gameObject); // Destruye el enemigo
         }
     }
 
-}
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Aliado")
+        {
+            combate.esta_en_combate = true;
+            // Obtiene una referencia al objeto del aliado
+            aliado = collision.gameObject.GetComponent<Vida_aliado_dummy>();
+        }
+    }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Aliado")
+        {
+            combate.esta_en_combate = false;
+        }
+    }
+}
--- Assets/__TESTING__/Boss_3_Pruebas/Vida_aliado_dummy.cs
+++ Assets/__TESTING__/Boss_3_Pruebas/Vida_aliado_dummy.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Vida_aliado_dummy : MonoBehaviour
 {
     private bool combate_aliado = false;
+    public float vida_Maxima = 50f; // La vida del enemigo
     public float vida = 50f; // La vida del enemigo
     public float dps = 5f;
+    private Boss3_vida enemigo;
 
     private void Update()
     {
         if (combate_aliado == true)
         {
-            // Obtiene una referencia al objeto del enemigo
-            Boss3_vida enemigo = GameObject.FindObjectOfType<Boss3_vida>();
-
             // Accede a la variable daño_boss3 del enemigo
             recibirDaño(enemigo.daño_boss);
         }
     }
+
 
     private void recibirDaño(float daño)
     {
@@ -38,6 +38,15 @@ public class Vida_aliado_dummy : MonoBehaviour
         if (collision.gameObject.tag == "Enemigo")
         {
             combate_aliado = true;
+            // Obtiene una referencia al objeto del aliado
+            enemigo = collision.gameObject.GetComponent<Boss3_vida>();
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            combate_aliado= false;
         }
     }
 }
