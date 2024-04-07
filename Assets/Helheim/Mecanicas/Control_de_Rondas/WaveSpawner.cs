4a214fd62733371647ee21ff3e996e73f68a02ae
arielbej <ariel507182003@gmail.com>
2024-04-08 01:25:26 +0200

    Escena hecha, falta arreglar el wavespawner

--- Assets/Helheim/Mecanicas/Control_de_Rondas/WaveSpawner.cs
+++ Assets/Helheim/Mecanicas/Control_de_Rondas/WaveSpawner.cs
@@ -9,7 +9,9 @@ public class WaveSpawner : MonoBehaviour
     public int _currentWaveIndex; // Inicializa a 0
     private int _enemiesLeftToSpawn;
     public int _activeEnemies; // Nuevo contador
-    public GameObject _lastEnemy; // Referencia al ъltimo enemigo
+    public GameObject _lastEnemy; // Referencia al último enemigo
+
+    private bool _firstWaveLaunched = false;
 
     private void Start()
     {
@@ -18,7 +20,7 @@ public class WaveSpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemyInWave()
     {
-        if (_enemiesLeftToSpawn > 0)
+        if (_enemiesLeftToSpawn > 0 && _currentEnemyIndex < _waves[_currentWaveIndex].WaveSettings.Length)
         {
             yield return new WaitForSeconds(_waves[_currentWaveIndex]
                 .WaveSettings[_currentEnemyIndex]
@@ -33,29 +35,49 @@ public class WaveSpawner : MonoBehaviour
 
             StartCoroutine(SpawnEnemyInWave());
         }
-        else if (_currentWaveIndex < _waves.Length - 1) // Solo incrementa si no estamos en la última ola
-        {
-            _currentWaveIndex++;
-            LaunchWave(); // Lanza la siguiente ola
-        }
         else
         {
-            // Detener el juego
-            _currentWaveIndex++;
-            Time.timeScale = 0f;
+            CheckWaveCompletion(); // Verifica si la ola actual ha sido completada
         }
     }
 
     public void LaunchWave()
     {
-        if (_activeEnemies == 0 && _currentWaveIndex < _waves.Length) // Solo lanza la siguiente ola si no hay enemigos activos
+        if (!_firstWaveLaunched)
+        {
+            _firstWaveLaunched = true;
+        }
+        else
+        {
+            // Verifica si estamos en la última ronda antes de incrementar _currentWaveIndex
+            if (_currentWaveIndex < _waves.Length - 1)
+            {
+                _currentWaveIndex++; // Incrementa la ronda solo si no es la última
+            }
+        }
+
+        if (_currentWaveIndex < _waves.Length) // Lanza la siguiente ola si hay olas disponibles
         {
             _enemiesLeftToSpawn = _waves[_currentWaveIndex].WaveSettings.Length;
-            _currentEnemyIndex = 0; // Reinicia _currentEnemyIndex
-            if (this != null)
+            _currentEnemyIndex = 0;
+            if (this != null) // Verifica si el objeto WaveSpawner todavía existe
             {
                 StartCoroutine(SpawnEnemyInWave());
             }
+        }
+    }
+
+    private void CheckWaveCompletion()
+    {
+        if (_activeEnemies == 0 && _currentWaveIndex < _waves.Length - 1 && !_waves[_currentWaveIndex].IsCompleted) // Solo incrementa si no estamos en la última ola, no hay enemigos activos y la ronda actual no está completada
+        {
+            _waves[_currentWaveIndex].IsCompleted = true; // Marca la ronda como completada
+            LaunchWave(); // Lanza la siguiente ola
+        }
+        else if (_activeEnemies == 0 && _currentWaveIndex == _waves.Length - 1) // Si estamos en la última ola y todos los enemigos han sido destruidos
+        {
+            // Detener el juego
+            Time.timeScale = 0f;
         }
     }
 
@@ -73,6 +95,7 @@ public class Waves
 {
     [SerializeField] private WaveSettings[] _waveSettings;
     public WaveSettings[] WaveSettings { get => _waveSettings; }
+    public bool IsCompleted;
 }
 
 [System.Serializable]
