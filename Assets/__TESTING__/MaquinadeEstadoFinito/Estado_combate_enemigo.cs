a0859f2fab462ef414420f43443605c9423d0e70
arielbej <ariel507182003@gmail.com>
2024-04-22 17:01:40 +0200

    Commit Debugin 2

--- Assets/__TESTING__/MaquinadeEstadoFinito/Estado_combate_enemigo.cs
+++ Assets/__TESTING__/MaquinadeEstadoFinito/Estado_combate_enemigo.cs
@@ -19,9 +19,6 @@ public class Estado_combate_enemigo : Estado_Base_Enemigo
     public override void EnterState(Controlador_de_Estados enemigo)
     {
         Debug.Log("HOLA DESDE EL ESTADO COMBATE");
-        ///GameObject characterManagerObject = GameObject.Find("Game Manager");
-        //cartas = characterManagerObject.GetComponent<CharacterCardManager>();
-
     }
 
     public override void UpdateState(Controlador_de_Estados enemigo)
@@ -34,7 +31,7 @@ public class Estado_combate_enemigo : Estado_Base_Enemigo
         {
             dano_recibido = aliado.dps;
             vida -= dano_recibido * Time.deltaTime;
-            Debug.Log("DAÑO RECIBIDO : " + dano_recibido);
+            //Debug.Log("DAÑO RECIBIDO : " + dano_recibido);
 
             if (vida <= 0 || dano_recibido >= vidaMaxima)
             {
@@ -57,51 +54,5 @@ public class Estado_combate_enemigo : Estado_Base_Enemigo
     }
 
 }
-    //----------------------------------------------------------------------------
-    //DROPEO DE CARTAS
-    //----------------------------------------------------------------------------
-    /*
-    public void cartaAleatoria()
-    {
-
-        cartas.amtOfCards++;
-
-        if (GenerateRandomNumber() == 1)
-        {
-            cartas.characterCardSO[cartas.amtOfCards - 1] = martillo;
-        }
-        else
-        {
-            cartas.characterCardSO[cartas.amtOfCards - 1] = berserk;
-        }
-
-        cartas.characterCards = new GameObject[cartas.amtOfCards];
-        cartas.AddCharacterCard(cartas.amtOfCards - 1);
-    }
-
-    int GenerateRandomNumber()
-    {
-        // Genera un número aleatorio entre 0 (inclusive) y 1 (exclusivo)
-        float randomNumber = Random.value;
-
-        // Si el número generado es menor o igual a 0.5, devuelve 1; de lo contrario, devuelve 2
-        if (randomNumber <= 0.5f)
-        {
-            return 1;
-        }
-        else
-        {
-            return 2;
-        }
-    }
-    bool DropCarta()
-    {
-        // Genera un número aleatorio entre 0 (inclusive) y 1 (exclusivo)
-        float randomNumber = Random.value;
-
-        // Si el número generado es menor o igual a 0.2, devuelve verdadero; de lo contrario, devuelve falso
-        return randomNumber <= 0.2f;
-    }
-    */
 
 
