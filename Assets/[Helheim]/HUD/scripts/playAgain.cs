8c66e16e8ca684fd92ff0fa2a918d032c690f678
arielbej <ariel507182003@gmail.com>
2024-04-23 02:29:10 +0200

    Ultimos arreglos para el sprint y un poco de balanceo
    He intentado balancear un poco los aliados. Aparte de eso el juego esta bien.

---
+++ Assets/[Helheim]/HUD/scripts/playAgain.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CambiarLaEscena : MonoBehaviour
+{
+    // M�todo para cambiar de escena al hacer clic en el bot�n
+    public void Quit()
+    {
+            SceneManager.LoadScene("MenuInicial");
+
+        Debug.Log("Cambio de pantalla!");
+
+    }
+    public void PlayAgain()
+    {
+            SceneManager.LoadScene("Main");
+
+        Debug.Log("Cambio de pantalla!");
+
+    }  
+   
+}
